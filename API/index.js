ae16aa5a6588efd9ca653b82a2d82dd3b4360d59
Paula Pizarro <paula.pizarro.r@outlook.com>
2020-04-19 22:16:03 -0400

    Finalización del CRUD básico de REST API para los pacientes de la veterinaria usando NodeJS, Express y MongoDB

---
+++ API/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+/* Crear el servidor */
+const app = express();
+
+/* Conectar a MongoDB */
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+/* Habilitar el body-parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/* Habilitar routing */
+app.use('/', routes())
+
+
+/* Puerto y arrancar el servidor */
+app.listen(4000, ()=> {
+    console.log('Servidor funcionando');
+})
