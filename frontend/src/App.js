d6dcc51f0661eb0a3b7a6d51c2d646049c439a4e
Paula Pizarro <paula.pizarro.r@outlook.com>
2020-04-20 19:53:34 -0400

    Proyecto finalizado, parte frontend con React, API-rest con Axios.

---
+++ frontend/src/App.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import clienteAxios from './config/axios'
+
+/* Components */
+import Pacientes from './components/Pacientes';
+import NuevaCita from './components/NuevaCita';
+import Cita from './components/Cita';
+
+function App() {
+
+    /* State de la app */
+    const [citas, guardarCitas] = useState([]);
+    const [consultar, guardarConsultar] = useState(true);
+
+    useEffect( () => {
+        if (consultar) {
+            const consultarAPI = () => {
+                clienteAxios.get('/pacientes')
+                    .then(res => {
+                        /* Colocar en el state el resultado */
+                        guardarCitas(res.data);
+
+                        /* Deshabilitar la consulta */
+                        guardarConsultar(false);
+                    })
+                    .catch (err => {
+                        console.log(err);
+                    })
+            }
+            consultarAPI();
+        }
+    }, [consultar]);
+
+    return (
+        <Router>
+            <Switch>
+                <Route 
+                    exact path="/"
+                    component={() => 
+                        <Pacientes 
+                            citas={citas}
+                        />}
+                />
+
+                <Route 
+                    exact path="/nueva"
+                    component={() => 
+                        <NuevaCita 
+                            guardarConsultar={guardarConsultar}
+                        />}
+                />
+
+                <Route 
+                    exact path="/cita/:id"
+                    render={(props) => {
+                        const cita = citas.filter(cita => cita._id === props.match.params.id)
+
+                        return (
+                            <Cita 
+                                cita={cita[0]}
+                                guardarConsultar={guardarConsultar}
+                            />
+                        )
+                    }}
+                />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
